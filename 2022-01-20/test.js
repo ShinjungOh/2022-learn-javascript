cf9e668ab5ee5ec155ab4a899b7aa64da05bfca4
ShinjungOh <tlswjd9260@gmail.com>
2022-01-20 17:57:59 +0900

    [ADD] 모던 자바스크립트 변수

---
+++ 2022-01-20/test.js
@@ -0,0 +1,23 @@
+/*
+let message = "Hello!";
+
+let user = "John",
+    age = "25";
+
+message = "Hello!2";
+
+console.log(message);
+ */
+
+let Hello = "Hello World";
+let message = Hello;
+
+console.log(Hello);
+console.log(message);
+
+const myBirthday = '19.06.1995';
+console.log(myBirthday);
+
+const COLOR_RED = "F00";
+let color = COLOR_RED;
+console.log(color);
---
+++ 2022-01-20/test2.js
@@ -0,0 +1,9 @@
+let admin;
+let name = "John";
+
+admin = name;
+console.log(admin);
+
+let livePlanet = "Earth"
+
+let activeUserName = "sj"
