eb2445e441da42faa8a51c5a26459edf093ddb1d
ShinjungOh <tlswjd9260@gmail.com>
2022-01-25 23:36:48 +0900

    [ADD] 유노코딩 자바스크립트 boolean

---
+++ 2022-01-25/script.js
@@ -0,0 +1,48 @@
+let data;  //아직 정의되지 않음
+console.log(data);
+
+data = "Hello";  //문자열로 초기화
+console.log(data);
+
+data = null;  //데이터가 없다
+console.log(data);
+
+data = true;
+console.log(typeof data);
+
+data = false;
+console.log(typeof data);
+
+data = True;  //소문자로만 써야함
+console.log(data);
+
+
+/*
+복합 대입 연산자
+let result = 3;
+
+result += 3;    //result + 3;
+console.log(result);
+
+result -= 2;
+console.log(result);
+*/
+
+/*
+let result = (1+1)/2*3+3;
+console.log(result);
+result = 1-1;
+console.log(result);
+result = 1*1;
+console.log(result);
+result = 1/2;
+console.log(result);
+*/
+
+/*
+console.log(3+4);
+console.log(3-4);
+console.log(3*4);
+console.log(3/4);
+console.log(3%4);
+*/
