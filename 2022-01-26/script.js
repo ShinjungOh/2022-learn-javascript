87451027e07283850aa9c43abb2232e9695ca677
ShinjungOh <tlswjd9260@gmail.com>
2022-01-26 03:57:48 +0900

    [ADD] 유노코딩 자바스크립트 document

---
+++ 2022-01-26/script.js
@@ -0,0 +1,20 @@
+/*
+console.log(document.querySelector("h1"))
+console.log(document.querySelector("p"))
+console.log(document.querySelector("#text"))
+console.log(document.querySelector(".paragraph"))
+*/
+
+/*
+console.log(document.getElementById("text"));
+console.log(document.getElementById("p"));
+*/
+
+const h1 = document.querySelector("h1")
+const p = document.getElementById("text")
+
+console.log(h1.textContent);
+h1.textContent = "Hello!!"
+
+p.textContent = "겟엘리먼트바이아이디"
+console.log(p.textContent);
