19a395fd0de6a6156b6e0140f72ba4dd562755c8
ShinjungOh <tlswjd9260@gmail.com>
2022-01-28 02:29:19 +0900

    [ADD] 유노코딩 자바스크립트 조건문

---
+++ 2022-01-28/script.js
@@ -0,0 +1,18 @@
+let num1 = '3';
+let num2 = 3;
+
+console.log(num1 != num2);
+console.log(num1 !== num2);
+//boolean 데이터를 반환
+
+
+let number = 5;
+if(number == 3){
+    console.log("조건으로 사용된 비교연산식이 참을 반환했다!")
+}else{
+    console.log("거짓")
+}
+
+console.log("독립적으로 사용된 콘솔로그")
+
+
