c96bf8215190ca569d2f11a8e979aa7dc0136ed6
ShinjungOh <tlswjd9260@gmail.com>
2022-02-07 23:54:58 +0900

    [ADD] 유노코딩 자바스크립트 삼항 연산

---
+++ 2022-02-07/script3.js
@@ -0,0 +1,20 @@
+/*
+let result;
+3 > 2 ? "true" : "false"
+
+result = 3 > 2 ? "true" : "false"
+console.log(result)
+ */
+
+
+const select = document.querySelector("select")
+const button = document.querySelector("button")
+
+button.addEventListener("click", function () {
+    console.log(select.value)
+    let result;
+    result = select.value == "foot" ?
+        "축구를 선택했네요" :
+        "축구를 선택하지 않았네요"
+    alert(result)
+})
