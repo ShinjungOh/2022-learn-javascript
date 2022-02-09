98e969f9e84d1945e7d804a91a2945806c977c3c
ShinjungOh <tlswjd9260@gmail.com>
2022-02-09 14:56:12 +0900

    [ADD] 유노코딩 자바스크립트 Math 객체

---
+++ 2022-02-09/script.js
@@ -0,0 +1,9 @@
+/*
+console.log(Math.random()) //0이상 1미만
+let landomNum = Math.floor(Math.random() * 10) //10미만
+console.log(landomNum)
+ */
+
+let number = 100
+let powNum = Math.pow(10, 4)
+console.log(Math.sqrt(powNum))
