561b394866cb691d2176001926762a503f909607
ShinjungOh <tlswjd9260@gmail.com>
2022-04-07 22:13:43 +0900

    [ADD] game field 생성

--- 2022-04-04/project_game/script.js
+++ 2022-04-04/project_game/script.js
@@ -2,7 +2,9 @@ const playBtn = document.querySelector('.playBtn');
 const stopBtn = document.querySelector('.stopBtn');
 const timer = document.querySelector('.timer');
 const score = document.querySelector('.score');
-const replayBtn = document.querySelector('.replayBtn')
+const replayBtn = document.querySelector('.replayBtn');
+const field = document.querySelector('.game_field');
+const fieldRect = field.getBoundingClientRect();
 
 stopBtn.style.display = "none"
 playBtn.addEventListener('click', () => {
@@ -24,12 +26,15 @@ let isStop = true;
 let time;
 
 const countDown = () => {
-    console.log(currentSecond);
+    console.log(`${currentSecond}초`);
     currentSecond -= 1;
     timer.innerHTML = currentSecond;
     if (currentSecond === 0) {
         isStop = true;
         clearInterval(time);
+        console.log('0!');
+        stopBtn.style.display = "none"
+        playBtn.style.display = "block"
     }
 };
 
@@ -45,4 +50,37 @@ const stop = () => {
     clearInterval(time);
     isStop = true;
 };
+
+
+const carrotSize = 80;
+
+function initGame() {
+    //console.log(fieldRect);
+    createGameItem('carrot', '5', 'img/carrot.png');
+    createGameItem('bug', '5', 'img/bug.png');
+}
+
+function createGameItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width - carrotSize;
+    const y2 = fieldRect.height - carrotSize;
+    for (let i = 0; i < count; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute';
+        const x = randomNum(x1, x2);
+        const y = randomNum(y1, y2);
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`;
+        field.appendChild(item);
+    }
+}
+
+function randomNum(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+initGame();
 
