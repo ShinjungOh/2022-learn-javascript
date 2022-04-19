0989cec7bd39214091e551e96b25588372984768
ShinjungOh <tlswjd9260@gmail.com>
2022-04-19 20:13:54 +0900

    [ADD] 모던 자바스크립트 primitives methods

---
+++ 2022-04-18/primitives-methods.js
@@ -0,0 +1,15 @@
+//https://ko.javascript.info/primitives-methods
+
+let str = "Hello";
+console.log(str.toUpperCase());
+
+let n = 1.23456;
+console.log(n.toFixed(2));
+
+let num = "123";
+console.log(typeof num)
+let number = Number("123");
+console.log(typeof number)
+
+//null, undefined는 메서드가 없다.
+
