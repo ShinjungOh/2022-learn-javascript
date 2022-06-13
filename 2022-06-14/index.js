15dfee93f4a9393168c2524e3dbd91868cd25cdd
ShinjungOh <tlswjd9260@gmail.com>
2022-06-14 04:08:38 +0900

    [ADD] node.js 패키지 생성

---
+++ 2022-06-14/index.js
@@ -0,0 +1,9 @@
+console.log("Hello nodejs package");
+
+const randomColor = require('randomcolor');
+
+let colro1 = randomColor();
+let colro2 = randomColor();
+let colro3 = randomColor();
+
+console.log(colro1, colro2, colro3);
